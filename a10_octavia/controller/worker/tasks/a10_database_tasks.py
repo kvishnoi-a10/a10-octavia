bdbe5003deea1003cbe321a206bb65c25fc9df86
hvenkatesan-a10 <hvenkatesan@a10networks.com>
2025-12-02 14:38:32 +0530

    temperory change of hardcoded password

--- a10_octavia/controller/worker/tasks/a10_database_tasks.py
+++ a10_octavia/controller/worker/tasks/a10_database_tasks.py
@@ -207,9 +207,11 @@ class GetVThunderByLoadBalancer(BaseDatabaseTask):
             if vthunder:
                 if flag:
                     secret_name = loadbalancer.get(constants.PROJECT_ID) + '_default_vthunder_password'
-                    vthunder.password = a10_task_utils.get_password(barbican_client, loadbalancer.get(constants.PROJECT_ID), secret_name)
+                    vthunder.password = "YTEw"
+                    #vthunder.password = a10_task_utils.get_password(barbican_client, loadbalancer.get(constants.PROJECT_ID), secret_name)
                 else:
-                    vthunder.password = a10_task_utils.get_password(barbican_client, loadbalancer.get(constants.PROJECT_ID))
+                    vthunder.password = "QTEwbmV0d29ya3Mh"
+                    #vthunder.password = a10_task_utils.get_password(barbican_client, loadbalancer.get(constants.PROJECT_ID))
                 vthunder.password = a10_task_utils.decode_base64(vthunder.password)
                 
             if not master_amphora_status:
